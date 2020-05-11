7bf1b46776b796d82d5e67adf9fd470a16f9b7b4
이은덕 <pleed0215@ieundeog-ui-iMac.local>
2020-05-11 15:16:54 +0900

    third commit

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -9,9 +9,11 @@ import Home from "Routes/Home";
 import Search from "Routes/Search";
 import TV from "Routes/TV";
 import Detail from "Routes/Detail";
+import Header from "Components/Header";
 
 export default () => (
   <Router>
+    <Header />
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/search" component={Search} />
