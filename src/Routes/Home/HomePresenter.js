0bbca688385c3638cea709cf3cf8e1e3670bec71
pleed0215 <pleed0215@icloud.com>
2020-05-14 00:46:39 +0900

    nineth commit

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,42 +1,41 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-
 import Section from "Components/Section";
+import Loader from "Components/Loader";
 
 const Container = styled.div`
   padding: 0px 10px;
 `;
 
 const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading }) => {
-  return loading ? null : (
+  return loading ? (
+    <Loader />
+  ) : (
     <Container>
-      {nowPlaying && nowPlaying.length > 0 &&
-        (<Section title="Now Playing">
-          {nowPlaying.map(movie =>
-            <div>
-              {movie.title}
-            </div>)}
-        </Section>)
-      }
-      {popular && popular.length > 0 &&
-        (<Section title="Popular">
-          {popular.map(movie =>
-            <div>
-              {movie.title}
-            </div>)}
-        </Section>)
-      }
-      {upcoming && upcoming.length > 0 &&
-        (<Section title="Upcoming">
-          {upcoming.map(movie =>
-            <div>
-              {movie.title}
-            </div>)}
-        </Section>)
-      }
-    </Container >
-  )
+      {nowPlaying && nowPlaying.length > 0 && (
+        <Section title="Now Playing">
+          {nowPlaying.map((movie) => (
+            <div>{movie.title}</div>
+          ))}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular">
+          {popular.map((movie) => (
+            <div>{movie.title}</div>
+          ))}
+        </Section>
+      )}
+      {upcoming && upcoming.length > 0 && (
+        <Section title="Upcoming">
+          {upcoming.map((movie) => (
+            <div>{movie.title}</div>
+          ))}
+        </Section>
+      )}
+    </Container>
+  );
 };
 
 HomePresenter.propTypes = {
--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -2,48 +2,48 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import Section from "Components/Section";
+import Loader from "Components/Loader";
 
 const Container = styled.div`
-    &:not(:last-child) {
-        margin-bottom: 50px;
-    }
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 90%;
+  &:not(:last-child) {
+    margin-bottom: 50px;
+  }
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 90%;
 `;
 
 const TVPresenter = ({ topRated, popular, airingToday, error, loading }) => {
   console.log(popular);
-  return loading ? null : (
+  return loading ? (
+    <Loader />
+  ) : (
     <Container>
-      {topRated && topRated.length > 0 &&
-        (<Section title="Top Rated TV shows">
-          {topRated.map(tvShow =>
-            <div>
-              {tvShow.name}
-            </div>)}
-        </Section>)
-      }
-      {popular && popular.length > 0 &&
-        (<Section title="Popular TV shows">
-          {popular.map(tvShow =>
-            <div>
-              {tvShow.name}
-            </div>)}
-        </Section>)
-      }
-      {airingToday && airingToday.length > 0 &&
-        (<Section title="Airing TV show today">
-          {airingToday.map(tvShow =>
-            <div>
-              {tvShow.name}
-            </div>)}
-        </Section>)
-      }
-    </Container >
-  )
+      {topRated && topRated.length > 0 && (
+        <Section title="Top Rated TV shows">
+          {topRated.map((tvShow) => (
+            <div>{tvShow.name}</div>
+          ))}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular TV shows">
+          {popular.map((tvShow) => (
+            <div>{tvShow.name}</div>
+          ))}
+        </Section>
+      )}
+      {airingToday && airingToday.length > 0 && (
+        <Section title="Airing TV show today">
+          {airingToday.map((tvShow) => (
+            <div>{tvShow.name}</div>
+          ))}
+        </Section>
+      )}
+    </Container>
+  );
 };
 
 TVPresenter.propTypes = {
