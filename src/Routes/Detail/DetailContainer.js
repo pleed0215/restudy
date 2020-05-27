5029d0cd067197480bd4aaaeb29f9419687944ba
이은덕 <pleed0215@ieundeog-ui-iMac.local>
2020-05-27 11:19:16 +0900

    detail job almost done

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -3,65 +3,65 @@ import DetailPresenter from "./DetailPresenter";
 import { tvApi, movieApi } from "api";
 
 export default class extends React.Component {
-    constructor(props) {
-        super(props);
-        const { match: { path } } = this.props;
+  constructor(props) {
+    super(props);
+    const {
+      match: { path },
+    } = this.props;
 
-        this.state = {
-            result: null,
-            error: null,
-            loading: true,
-            isMovie: path.includes("/movie/"),
-        }
+    this.state = {
+      result: null,
+      error: null,
+      loading: true,
+      isMovie: path.includes("/movie/"),
+    };
+  }
+
+  async componentDidMount() {
+    console.log("props", this.props);
+    const {
+      match: {
+        params: { id },
+      },
+      history: { push },
+    } = this.props;
+    const { isMovie } = this.state;
+    const parsedId = parseInt(id);
+
+    if (isNaN(parsedId)) {
+      return push("/");
     }
 
+    let result = null;
 
-    async componentDidMount() {
-        console.log('props', this.props);
-        const {
-            match:
-            {
-                params:
-                { id },
-            },
-            history:
-            {
-                push
-            }
-        } = this.props;
-        const { isMovie } = this.state;
-        const parsedId = parseInt(id);
-
-        if (isNaN(parsedId)) {
-            return push('/');
-        }
-
-        let result = null;
-
-        try {
-            if (isMovie) {
-                result = await movieApi.detail(parsedId);
-            } else {
-                result = await tvApi.detail(parsedId);
-            }
-        } catch {
-            this.setState({ error: "Can't get detail information of movie(or tv show)." });
-        } finally {
-            this.setState({
-                result: result.data,
-                loading: false
-            })
-        }
+    try {
+      if (isMovie) {
+        result = await movieApi.detail(parsedId);
+      } else {
+        result = await tvApi.detail(parsedId);
+      }
+    } catch {
+      this.setState({
+        error: "Can't get detail information of movie(or tv show).",
+      });
+    } finally {
+      this.setState({
+        result: result.data,
+        loading: false,
+      });
     }
+  }
 
-    render() {
-        const {
-            result, error, loading
-        } = this.state;
-        console.log(this.state);
-        return <DetailPresenter
-            result={result}
-            error={error}
-            loading={loading} />
-    }
+  render() {
+    const { result, error, loading, isMovie } = this.state;
+    console.log(this.state);
+    return (
+      <DetailPresenter
+        result={result}
+        error={error}
+        loading={loading}
+        isMovie={isMovie}
+      />
+    );
+  }
 }
--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -11,11 +11,11 @@ const Container = styled.div`
 
 const Backdrop = styled.div`
   position: absolute;
-  left:0;
-  top:-30px;
+  left: 0;
+  top: -30px;
   width: 100%;
   height: 100%;
-  background-image: url(${props => props.bgUrl});
+  background-image: url(${(props) => props.bgUrl});
   background-size: cover;
   background-position: center center;
   filter: blur(3px);
@@ -31,26 +31,102 @@ const Content = styled.div`
   padding: 20px;
 `;
 const Cover = styled.div`
-  background-image: url(${props => props.bgUrl});
+  background-image: url(${(props) => props.bgUrl});
   background-size: cover;
   background-position: center center;
   width: 30%;
   height: 100%;
+  border-radius: 5px;
 `;
 
-const DetailPresenter = ({ result, error, loading }) =>
-  loading ? <Loader /> :
-    <Container>
-      <Backdrop bgUrl={`https://image.tmdb.org/t/p/original/${result.backdrop_path}`} />
-      <Content>
-        <Cover bgUrl={result.poster_path ? `https://image.tmdb.org/t/p/original/${result.poster_path}` : require("../../assets/noposter.jpg")} />
-      </Content>
-    </Container>
+const Data = styled.div`
+  width: 70%;
+  margin-left: 20px;
+  display: flex;
+  flex-direction: column;
+`;
+const Title = styled.h2`
+  font-size: 24px;
+  font-weight: 600;
+  margin-bottom: 10px;
+`;
+const UnderTitle = styled.ul`
+  margin-bottom: 20px;
+`;
+const UnderTitleItem = styled.li`
+  float: left;
+  position: relative;
+  left: 10px;
+  &:not(:first-child) {
+    margin-left: 20px;
+    list-style-type: disc;
+  }
+  &:not(:first-child) span {
+    margin-left: 20px;
+  }
+  &:not(:last-child) {
+    margin-right: 30px;
+  }
+`;
+const Overview = styled.p``;
+
+const DetailPresenter = ({ result, error, loading, isMovie }) =>
+  loading ? (
+    <Loader />
+  ) : (
+    result && (
+      <Container>
+        <Backdrop
+          bgUrl={`https://image.tmdb.org/t/p/original/${result.backdrop_path}`}
+        />
+        <Content>
+          <Cover
+            bgUrl={
+              result.poster_path
+                ? `https://image.tmdb.org/t/p/original/${result.poster_path}`
+                : require("../../assets/noposter.jpg")
+            }
+          />
+          <Data>
+            <Title>{isMovie ? result.title : result.name}</Title>
+            <UnderTitle>
+              <UnderTitleItem>
+                <span>
+                  {isMovie
+                    ? result.release_date.substring(0, 4)
+                    : result.first_air_date.substring(0, 4)}
+                </span>
+              </UnderTitleItem>
+              <UnderTitleItem>
+                <span>
+                  {isMovie ? result.runtime : result.episode_run_time}min
+                </span>
+              </UnderTitleItem>
+              <UnderTitleItem>
+                <span>
+                  {result.genres.map((genre, idx) =>
+                    result.genres.length - 1 !== idx
+                      ? `${genre.name}/`
+                      : genre.name
+                  )}
+                </span>
+              </UnderTitleItem>
+              <UnderTitleItem>
+                <span>{result.vote_average}</span>
+              </UnderTitleItem>
+            </UnderTitle>
+            <Overview>{result.overview}</Overview>
+          </Data>
+        </Content>
+      </Container>
+    )
+  );
 
 DetailPresenter.propTypes = {
   result: PropTypes.object,
   error: PropTypes.string,
   loading: PropTypes.bool.isRequired,
+  isMovie: PropTypes.bool.isRequired,
 };
 
 export default DetailPresenter;
