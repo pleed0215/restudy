fdd3c8159125e68236bb8dc823ba3749f36d9831
이은덕 <pleed0215@ieundeog-ui-iMac.local>
2020-05-28 14:14:17 +0900

    Updates

---
+++ precache-manifest.3084d9a153785596c7f0fbc351a86c06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70176113e701b1319056cca2eb21b685",
+    "url": "/restudy/index.html"
+  },
+  {
+    "revision": "ad7f1269c493f0166c37",
+    "url": "/restudy/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "ad7f1269c493f0166c37",
+    "url": "/restudy/static/js/2.31e35524.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/restudy/static/js/2.31e35524.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00cc16257cacf1d5d54d",
+    "url": "/restudy/static/js/main.0d5a1723.chunk.js"
+  },
+  {
+    "revision": "74ea84068f362af68af9",
+    "url": "/restudy/static/js/runtime-main.9f8fff88.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fuckflix/precache-manifest.00623c73557193f13c8ad79a90a122ed.js"
+  "/restudy/precache-manifest.3084d9a153785596c7f0fbc351a86c06.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fuckflix/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/restudy/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
