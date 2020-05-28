bfbc7ac79154bba1f30f472713982b07c23420e1
이은덕 <pleed0215@ieundeog-ui-iMac.local>
2020-05-28 13:55:36 +0900

    Updates

---
+++ precache-manifest.b11b31e6ada431f306364237a94373de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7d0af88427264f7965de4fba31e5b8",
+    "url": "/fuckflix/index.html"
+  },
+  {
+    "revision": "2bfd59aa1003fdb0d1aa",
+    "url": "/fuckflix/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "2bfd59aa1003fdb0d1aa",
+    "url": "/fuckflix/static/js/2.baf3512f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fuckflix/static/js/2.baf3512f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14fac222bb29747f6947",
+    "url": "/fuckflix/static/js/main.14f2bbf2.chunk.js"
+  },
+  {
+    "revision": "c3803d928e633a26acd0",
+    "url": "/fuckflix/static/js/runtime-main.753bcf03.js"
+  }
+]);
